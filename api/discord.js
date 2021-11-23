39a516a9bc8eb3bf213dde498194fcba596bb92a
Romans96 <24mattiaromagnoli96@gmail.com>
2021-11-23 18:17:05 +0100

    First beta commit

--- api/discord.js
+++ api/discord.js
@@ -3,7 +3,9 @@ const express = require("express");
 const router = express.Router();
 const fetch = require("node-fetch");
 const btoa = require("btoa");
-const { catchAsync } = require("../utils");
+const { 
+    catchAsync, getGuildBanner, getBasicAuth, apiAuth
+} = require("../utils");
 
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
@@ -23,50 +25,43 @@ router.get(
         // console.log(req.query.code)
         const code = req.query.code;
         // console.log("Code:", code, CLIENT_ID, CLIENT_SECRET);
-        const response = await fetch(`https://discord.com/api/oauth2/token`, {
-            method: "POST",
-            body: new URLSearchParams({
-                client_id: CLIENT_ID,
-                client_secret: CLIENT_SECRET,
-                code: code,
-                grant_type: "authorization_code",
-                redirect_uri: redirect,
-            }),
-            headers: {
-                'Cache-Control': 'no-cache',
-                'Content-Type': 'application/x-www-form-urlencoded',
-            },
-        });
-        const rsponsejson = await response.json();
-        // console.log(rsponsejson)
+
+        let basicAuth = await getBasicAuth(CLIENT_ID, CLIENT_SECRET, code, redirect)
+        if (basicAuth.error != null) return res.redirect("/error");
+        else basicAuth = basicAuth.res;
+
+        const basicAuthData = await basicAuth.json();
         
-        const user_infos_nojson = await fetch(
-            "https://discord.com/api/users/@me",
-            {
-                headers: {
-                    'Cache-Control': 'no-cache',
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                    authorization: `${rsponsejson.token_type} ${rsponsejson.access_token}`,
-                },
-            }
-        ),
-        user_infos = await user_infos_nojson.json();
+        let user_infos_nojson = await apiAuth("https://discord.com/api/users/@me", basicAuthData.token_type, basicAuthData.access_token )
+        if (user_infos_nojson.error != null) return res.redirect("/error");
+        else user_infos_nojson = user_infos_nojson.res;
+        
+        const user_infos = await user_infos_nojson.json();
         req.session.discord = {};
         req.session.discord.userInfo = user_infos;
 
-        const user_guilds_nojson = await fetch(
-            "https://discord.com/api/users/@me/guilds",
-            {
-                headers: {
-                    'Cache-Control': 'no-cache',
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                    authorization: `${rsponsejson.token_type} ${rsponsejson.access_token}`,
-                },
-            }
-        );
+        let user_guilds_nojson = await apiAuth("https://discord.com/api/users/@me/guilds", basicAuthData.token_type, basicAuthData.access_token )
+        if (user_guilds_nojson.error != null) return res.redirect("/error");
+        else user_guilds_nojson = user_guilds_nojson.res;
+        
         const user_guilds = await user_guilds_nojson.json();
         req.session.discord.userGuilds = user_guilds;
         req.session.discord.guildsCounter = Object.keys(user_guilds).length;
+        
+        // console.log(user_guilds[0])
+        // const t = await fetch(
+        //     "https://discordapp.com/guilds/"+user_guilds[0].id,
+        //     {
+        //         method: "GET",
+        //         headers: {
+        //             'Cache-Control': 'no-cache',
+        //             'Content-Type': 'application/x-www-form-urlencoded',
+        //             authorization: `${responsejson.token_type} ${responsejson.access_token}`,
+        //         },
+        //     }
+        // )
+        // console.log( await t.json())
+        await getGuildBanner(user_guilds[0].id, user_guilds[0].icon )
 
         res.redirect(`/servers`);
     })
--- utils.js
+++ utils.js
@@ -1,14 +1,80 @@
+const fetch = require("node-fetch");
 var utils = {
-// async/await error catcher
+    // async/await error catcher
     catchAsync: (fn) => (req, res, next) => {
         const routePromise = fn(req, res, next);
         if (routePromise.catch) {
             routePromise.catch((err) => next(err));
         }
     },
-    authentication: async () => {
 
-    }
-}
+    getBasicAuth: async (CLIENT_ID, CLIENT_SECRET, code, redirect) => {
+        try {
+            const res = await fetch(`https://discord.com/api/oauth2/token`, {
+                method: "POST",
+                body: new URLSearchParams({
+                    client_id: CLIENT_ID,
+                    client_secret: CLIENT_SECRET,
+                    code: code,
+                    grant_type: "authorization_code",
+                    redirect_uri: redirect,
+                }),
+                headers: {
+                    "Cache-Control": "no-cache",
+                    "Content-Type": "application/x-www-form-urlencoded",
+                },
+            });
+
+            if (res) return { error: null, res: res };
+            else {
+                console.log("Error on getBasicAuth function!", err);
+                return { error: "Error on getBasicAuth function!" };
+            }
+        } catch (err) {
+            console.log("Error on getBasicAuth function!", err);
+            return { error: "Error on getBasicAuth function!" };
+        }
+    },
+    apiAuth: async (url, token_type, access_token) => {
+        try {
+            const res = await fetch(url, {
+                headers: {
+                    'Cache-Control': 'no-cache',
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    authorization: `${token_type} ${access_token}`,
+                },
+            });
+
+            if (res) return { error: null, res: res };
+            else {
+                console.log("Error on apiAuth function!", err);
+                return { error: "Error on apiAuth function!" };
+            }
+        } catch (err) {
+            console.log("Error on apiAuth function!", err);
+            return { error: "Error on apiAuth function!" };
+        }
+    },
+
+    getGuildBanner: async (guildId, guildIconhash) => {
+        try {
+            // console.log(guildId, guildIconhash);
+            const guildBanner = await fetch(
+                "https://cdn.discordapp.com/icons/" +
+                    guildId +
+                    "/" +
+                    guildIconhash +
+                    ".png"
+            );
+            // console.log(guildBanner_nojson)
+            if (guildBanner && guildBanner.status == 200) {
+                return { error: null, image: guildBanner.url };
+            } else return { error: "Error on getGuildBanner" };
+        } catch (err) {
+            console.log("Error on getGuildBanner", err);
+            return { error: "Error on getGuildBanner" };
+        }
+    },
+};
 
 module.exports = utils;
